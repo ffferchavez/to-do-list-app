dc663dab872bf572a9a6be826ebf38cae40665b3
ffferchavez <138360629+ffferchavez@users.noreply.github.com>
2023-10-13 00:20:40 +0200

    Added code to files

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function newItem() {
+    // Adding a new item to the list of items:
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+  
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      $('#list').append(li);
+    }
+  
+    // Crossing out an item from the list of items:
+    li.on("dblclick", function() {
+      li.toggleClass("strike");
+    });
+  
+    // Adding the delete button "X":
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+  
+    crossOutButton.on("click", function() {
+      li.addClass("delete");
+    });
+  
+    // Reordering the items:
+    $('#list').sortable();
+  }
